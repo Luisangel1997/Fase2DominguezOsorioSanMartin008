824c829f9284d88cf9cdc88a7cc6fb3caf573ac4
Luisangel1997 <65382772+Luisangel1997@users.noreply.github.com>
2020-10-26 20:04:28 -0300

    Update models.py
    more

--- Modelos/models.py
+++ Modelos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse 
+import uuid
 
 # Create your models here.
+
+class Usuarios(models.Model):
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    telefono=models.IntegerField(max_length=9)
+
+	class Meta:
+		ordering = ['nombre']
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return self.nombre
+
+class Productos(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    nombreProductos=models.CharField(max_length=30)
+    descripcion=models.CharField(max_length=200)
+    precio=models.IntegerField(max_length=9)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+	class Meta:
+		ordering = ['id']
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return self.id
+
